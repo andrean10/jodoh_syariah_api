20f050e80437b0c5266b28173862355a4d958a13
Andrean <andreanramadhan11@gmail.com>
2023-03-02 00:50:27 +0700

    up: backup data from office

--- config/database/connection.js
+++ config/database/connection.js
@@ -1,7 +1,9 @@
 let Sequelize = require('sequelize');
-let conn = new Sequelize('jodoh_syariah', 'root', '', {
+let conn = new Sequelize('jodoh_syariah', 'root', 'root', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: "127.0.0.1",
+    connectionLimit: 10,
+    port: "8889",
 });
 
 // let conn = new Sequelize('0cissduqoH', '0cissduqoH', 'Ytee7z6rr4', {
